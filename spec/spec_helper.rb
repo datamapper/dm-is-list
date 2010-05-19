08acbe0bb60e0ee36a6816200bd4cd0f0fce71ad
snusnu <gamsnjaga@gmail.com>
2010-05-19 02:47:39 +0200

    Reorganized specs pass on all adapters/rubies
    See http://is.gd/cfnmf for the necessary commit to
    dm-transactions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,41 @@ require 'dm-migrations'
 
 DataMapper::Spec.setup
 
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+
+  has n, :todos
+end
+
+class Todo
+  include DataMapper::Resource
+
+  property :id,    Serial
+  property :title, String
+
+  belongs_to :user
+
+  is :list, :scope => [:user_id]
+end
+
+module TodoListHelper
+  ##
+  # Keep things DRY shortcut
+  #
+  #   todo_list.should == [ [1, 1], [2, 2], [3, 3], [4, 4], [5, 5] ]
+  #
+  #   todo_list(:user => @u2, :order => [:id]).should == [ [1, 1], [2, 2], [3, 3], [4, 4], [5, 5] ]
+  #
+  def todo_list(options={})
+    options = { :user => @u1, :order => [:position] }.merge(options)
+    Todo.all(:user => options[:user], :order => options[:order]).map{ |a| [a.id, a.position] }
+  end
+end
+
 Spec::Runner.configure do |config|
   config.extend(DataMapper::Spec::Adapters::Helpers)
+  config.send(:include, TodoListHelper)
 end
